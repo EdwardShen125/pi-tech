156e1a09047935ad1f5431a72fcdcdae23c2a7c7
EdwardShen125 <495430312@qq.com>
2025-03-02 19:34:43 +0800

    feat: 🎸 version
    add version for user entity

--- user/internal/infrastructure/repository/user/usermodel.go
+++ user/internal/infrastructure/repository/user/usermodel.go
@@ -3,11 +3,23 @@ package user
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"github.com/jinzhu/copier"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"github.com/zeromicro/go-zero/core/stringx"
+	"strings"
 	"user/internal/domain/entity"
 	"user/internal/domain/repository"
 )
+
+var userCASRowsWithPlaceHolder = strings.Replace(
+	strings.Join(
+		stringx.Remove(
+			userFieldNames,
+			"`id`", "`create_at`", "`create_time`", "`created_at`", "`update_at`", "`update_time`", "`updated_at`",
+		),
+		"=?,")+"=?",
+	"version = ?", "version = version + 1", 1)
 
 var _ repository.UserRepository = (*customUserModel)(nil)
 
@@ -55,7 +67,7 @@ func (m *customUserModel) Insert(ctx context.Context, e *entity.User) (int64, er
 	return res.LastInsertId()
 }
 
-func (m *customUserModel) FindOne(ctx context.Context, id int64) (*entity.User, error) {
+func (m *customUserModel) FindOne(ctx context.Context, id string) (*entity.User, error) {
 	po, err := m.defaultUserModel.FindOne(ctx, id)
 	if err != nil {
 		return nil, err
@@ -79,10 +91,13 @@ func (m *customUserModel) FindOneByName(ctx context.Context, name sql.NullString
 	return POToEntity(po), nil
 }
 
-func (m *customUserModel) Update(ctx context.Context, e *entity.User) error {
-	return m.defaultUserModel.Update(ctx, EntityToPO(e))
+func (m *customUserModel) Update(ctx context.Context, newData *entity.User) error {
+	po := EntityToPO(newData)
+	query := fmt.Sprintf("update %s set %s where `id` = ? and `version` = ? ", m.table, userCASRowsWithPlaceHolder)
+	_, err := m.conn.ExecCtx(ctx, query, po.Name, po.Password, po.Mobile, po.Gender, po.Nickname, po.Type, po.Id, po.Version)
+	return err
 }
 
-func (m *customUserModel) Delete(ctx context.Context, id int64) error {
+func (m *customUserModel) Delete(ctx context.Context, id string) error {
 	return m.defaultUserModel.Delete(ctx, id)
 }
--- user/internal/infrastructure/repository/user/usermodel_gen.go
+++ user/internal/infrastructure/repository/user/usermodel_gen.go
@@ -19,18 +19,18 @@ import (
 var (
 	userFieldNames          = builder.RawFieldNames(&User{})
 	userRows                = strings.Join(userFieldNames, ",")
-	userRowsExpectAutoSet   = strings.Join(stringx.Remove(userFieldNames, "`id`", "`create_at`", "`create_time`", "`created_at`", "`update_at`", "`update_time`", "`updated_at`"), ",")
+	userRowsExpectAutoSet   = strings.Join(stringx.Remove(userFieldNames, "`create_at`", "`create_time`", "`created_at`", "`update_at`", "`update_time`", "`updated_at`"), ",")
 	userRowsWithPlaceHolder = strings.Join(stringx.Remove(userFieldNames, "`id`", "`create_at`", "`create_time`", "`created_at`", "`update_at`", "`update_time`", "`updated_at`"), "=?,") + "=?"
 )
 
 type (
 	userModel interface {
 		Insert(ctx context.Context, data *User) (sql.Result, error)
-		FindOne(ctx context.Context, id int64) (*User, error)
+		FindOne(ctx context.Context, id string) (*User, error)
 		FindOneByMobile(ctx context.Context, mobile string) (*User, error)
 		FindOneByName(ctx context.Context, name sql.NullString) (*User, error)
 		Update(ctx context.Context, data *User) error
-		Delete(ctx context.Context, id int64) error
+		Delete(ctx context.Context, id string) error
 	}
 
 	defaultUserModel struct {
@@ -39,13 +39,15 @@ type (
 	}
 
 	User struct {
-		Id       int64          `db:"id"`
+		Id       string         `db:"id"`       // id
 		Name     sql.NullString `db:"name"`     // The username
 		Password string         `db:"password"` // The user password
 		Mobile   string         `db:"mobile"`   // The mobile phone number
-		Gender   string         `db:"gender"`   // gender,male|female|unknown
+		Gender   string         `db:"gender"`   // gender, male|female|unknown
 		Nickname string         `db:"nickname"` // The nickname
 		Type     int64          `db:"type"`     // The user type, 0:normal,1:vip, for test golang keyword
+		Balance  int64          `db:"balance"`  // user balance（单位分）
+		Version  int64          `db:"version"`  // Optimistic concurrency version
 		CreateAt sql.NullTime   `db:"create_at"`
 		UpdateAt time.Time      `db:"update_at"`
 	}
@@ -58,13 +60,13 @@ func newUserModel(conn sqlx.SqlConn) *defaultUserModel {
 	}
 }
 
-func (m *defaultUserModel) Delete(ctx context.Context, id int64) error {
+func (m *defaultUserModel) Delete(ctx context.Context, id string) error {
 	query := fmt.Sprintf("delete from %s where `id` = ?", m.table)
 	_, err := m.conn.ExecCtx(ctx, query, id)
 	return err
 }
 
-func (m *defaultUserModel) FindOne(ctx context.Context, id int64) (*User, error) {
+func (m *defaultUserModel) FindOne(ctx context.Context, id string) (*User, error) {
 	query := fmt.Sprintf("select %s from %s where `id` = ? limit 1", userRows, m.table)
 	var resp User
 	err := m.conn.QueryRowCtx(ctx, &resp, query, id)
@@ -107,14 +109,14 @@ func (m *defaultUserModel) FindOneByName(ctx context.Context, name sql.NullStrin
 }
 
 func (m *defaultUserModel) Insert(ctx context.Context, data *User) (sql.Result, error) {
-	query := fmt.Sprintf("insert into %s (%s) values (?, ?, ?, ?, ?, ?)", m.table, userRowsExpectAutoSet)
-	ret, err := m.conn.ExecCtx(ctx, query, data.Name, data.Password, data.Mobile, data.Gender, data.Nickname, data.Type)
+	query := fmt.Sprintf("insert into %s (%s) values (?, ?, ?, ?, ?, ?, ?, ?, ?)", m.table, userRowsExpectAutoSet)
+	ret, err := m.conn.ExecCtx(ctx, query, data.Id, data.Name, data.Password, data.Mobile, data.Gender, data.Nickname, data.Type, data.Balance, data.Version)
 	return ret, err
 }
 
 func (m *defaultUserModel) Update(ctx context.Context, newData *User) error {
 	query := fmt.Sprintf("update %s set %s where `id` = ?", m.table, userRowsWithPlaceHolder)
-	_, err := m.conn.ExecCtx(ctx, query, newData.Name, newData.Password, newData.Mobile, newData.Gender, newData.Nickname, newData.Type, newData.Id)
+	_, err := m.conn.ExecCtx(ctx, query, newData.Name, newData.Password, newData.Mobile, newData.Gender, newData.Nickname, newData.Type, newData.Balance, newData.Version, newData.Id)
 	return err
 }
 
